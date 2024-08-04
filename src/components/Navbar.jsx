3fbb072686473e836daf8cd6ae70ec1032801023
Niranjan bhat <111105882+NiranjanBhat123@users.noreply.github.com>
2024-08-04 14:32:10 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,42 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "../css/Navbar.css";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className="header">
       <nav className="nav container">
         <NavLink to="/" className="nav__logo" style={{ fontSize: "2rem" }}>
           Event Buzz
         </NavLink>
-
-        <div className="nav__menu" id="nav-menu">
+        <div className={`nav__menu ${isMenuOpen ? 'show-menu' : ''}`} id="nav-menu">
           <ul className="nav__list">
             <li className="nav__item">
-              <NavLink 
-                to="/" 
-                className={({ isActive }) => isActive ? "nav__link active" : "nav__link"} 
+              <NavLink
+                to="/"
+                className={({ isActive }) => isActive ? "nav__link active" : "nav__link"}
+                onClick={() => setIsMenuOpen(false)}
               >
                 Home
               </NavLink>
             </li>
             <li className="nav__item">
-              <NavLink 
-                to="/about" 
+              <NavLink
+                to="/about"
                 className={({ isActive }) => isActive ? "nav__link active" : "nav__link"}
+                onClick={() => setIsMenuOpen(false)}
               >
                 About Us
               </NavLink>
             </li>
-            <li>
-            <NavLink 
-                to="/host" 
+            <li className="nav__item">
+              <NavLink
+                to="/host"
                 className={({ isActive }) => isActive ? "nav__link active" : "nav__link"}
+                onClick={() => setIsMenuOpen(false)}
               >
                 Host an event
               </NavLink>
             </li>
           </ul>
+        </div>
+        <div className={`hamburger ${isMenuOpen ? 'active' : ''}`} onClick={toggleMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
         </div>
       </nav>
     </header>
